113a7998ad76f6b937b6011326ef1dc6d56f7c5a
Patrick Pruessen <pruessen@gmx.de>
2023-06-06 15:15:48 +0200

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "events#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "pages#home"
   resources :events do
     resources :bookings, only: :create
   end
