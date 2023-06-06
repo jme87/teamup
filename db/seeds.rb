03ca21e122769dc553d8c9852394217b66d21683
Marcela Moreau <moreau.marcela@gmail.com>
2023-06-06 12:42:39 +0200

    added faker to gemfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+require 'open-uri'
+
+puts "Seeding the database ....................................."
+
+#################### 10 hardcoded users ####################
+user1photo = URI.open("https://source.unsplash.com/DItYlc26zVI/600x300")
+user1 = User.new(
+  {
+    email: "brandon@test.com",
+    first_name: "Brandon",
+    last_name: Faker::Name.last_name,
+    user_name: "brandon",
+    bio: "Hey there, I'm Brandon, a 32-year-old tech enthusiast currently diving headfirst into the world of web development through LeWagon's Ruby on Rails Fullstack Web Development Bootcamp. When I'm not decoding the mysteries of JavaScript or weaving tales with HTML and CSS, you'll likely find me buried in a sci-fi novel or trying to replicate my grandmother's recipes - often with hilarious results (think spaghetti stuck on the ceiling!). I have a soft spot for anything turquoise or cobalt blue - I swear those colors fuel my creativity. My life philosophy? Code hard, laugh often, and never underestimate the power of a good home-cooked meal!",
+    city: "Berlin"
+    password: "123456"
+  }
+)
+user1.photo.attach(io: user1photo, filename: "user1.png", content_type: "image/png")
+user1.save!
