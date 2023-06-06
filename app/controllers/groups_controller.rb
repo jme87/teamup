1211bb8de42d307a0b0b2cf6d02cc76b3bac2c5a
loic khodarkovsky <lkhodarkovsky@gmail.com>
2023-06-06 12:39:34 +0200

    create action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,17 @@
 class GroupsController < ApplicationController
   def new
     @group = Group.new
+    @categories = ApplicationRecord::CATEGORIES
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.save
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:title, :description, :city, :category, :toggle_attribute, :photo)
   end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,5 @@
 class Group < ApplicationRecord
+
   belongs_to :user
   has_many :posts
   has_many :users, through: :user_groups
