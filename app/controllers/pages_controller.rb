f643a85f497bef5f94465a39bb543d33275e454f
jamie lord <hello@jamie-lord.com>
2023-06-07 15:03:32 +0200

    updated edit profile feature

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
-  def dashboard
-
-  end
-
   def landing_page
   #  @events = Event.all.sample(3)
     if request.location.city.present?
@@ -14,5 +10,25 @@ class PagesController < ApplicationController
     end
     @events = Event.near(@city, 10)
     # @events = Event.near("Tour Eiffel", 10)
+  end
+
+  def dashboard
+    # Pending events
+    @all_events_by_user = current_user.events
+
+    @my_pending_events = Booking.where(event_id: @all_events_by_user.pluck(:id), status: 'pending').select(:event_id).distinct
+
+    # Approved events
+    @my_approved_events = Booking.where(event_id: @all_events_by_user.pluck(:id), status: 'confirmed')
+    # Events I applied
+    sql_query = <<~SQL
+      bookings.user_id = :user_id AND bookings.status = 'pending'
+    SQL
+    @applied_events = Event.joins(:bookings).where(sql_query, user_id: current_user.id)
+    # Events I got approved
+    sql_query = <<~SQL
+      bookings.user_id = :user_id AND bookings.status = 'confirmed'
+    SQL
+    @approved_events = Event.joins(:bookings).where(sql_query, user_id: current_user.id)
   end
 end
