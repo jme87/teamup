bd9ba41af414679780d7ef76dc437ff61f98b9cf
loic khodarkovsky <lkhodarkovsky@gmail.com>
2023-06-08 12:54:02 +0200

    added tabs

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :bookings
+  has_many :user_groups
   has_many :events, through: :bookings
   has_many :groups, through: :user_groups
-  has_many :user_groups
   has_many :posts
 
   validates :first_name, presence: true
