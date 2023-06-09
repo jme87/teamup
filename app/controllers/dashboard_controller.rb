19fcc574b02288708eec144ee62eba2d07d573be
jamie lord <hello@jamie-lord.com>
2023-06-09 13:09:06 +0200

    created controller methods|

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,10 +6,16 @@ class DashboardController < ApplicationController
 
   def user_events
     @events = Event.all
+    @attending = Event.joins(:bookings).where(bookings: { user: current_user }) #.where(event: {'start_date > ?'Date.today})
+    @hosting = Event.where(user: current_user).where('start_date > ?', Date.today)
+    # @past = Event.where(user: current_user).where('start_date < ?', Date.today) +
   end
 
   def user_groups
     @events = Group.all
+    @organiser = Group.where(user: current_user)
+    @member = Group.joins(:user_groups).where(user_groups: { user: current_user })
+    @member = @member.select { |group| group.user_id == current_user.id }
   end
 
 end
