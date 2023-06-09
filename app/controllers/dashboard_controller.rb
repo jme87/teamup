b2867313cef12e78a053b5ad93e47d47ee3da29d
jamie lord <hello@jamie-lord.com>
2023-06-09 15:01:14 +0200

    tweaked dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,11 +2,13 @@ class DashboardController < ApplicationController
 
   def profile
     @events = Event.all
+    @attending = Event.joins(:bookings).where(bookings: { user: current_user }) #.where('event.start_date > ?', Date.today)
+    @member = Group.joins(:user_groups).where(user_groups: { user: current_user })
   end
 
   def user_events
     @events = Event.all
-    @attending = Event.joins(:bookings).where(bookings: { user: current_user }) #.where(event: {'start_date > ?'Date.today})
+    @attending = Event.joins(:bookings).where(bookings: { user: current_user }) #.where('event.start_date > ?', Date.today)
     @hosting = Event.where(user: current_user).where('start_date > ?', Date.today)
     # @past = Event.where(user: current_user).where('start_date < ?', Date.today) +
   end
